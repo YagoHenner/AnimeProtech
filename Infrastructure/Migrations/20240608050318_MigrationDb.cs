a7edcb8dacf0db14e69fde90db4a6807d5d1af23
YagoHenner <yagohenner@gmail.com>
2024-06-08 03:27:21 -0300

    mod: removendo docker-compose // feat: adicionando migration

---
+++ Infrastructure/Migrations/20240608050318_MigrationDb.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Diretor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Resumo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PalavrasChave",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PalavrasChave", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AnimePalavraChave",
+                columns: table => new
+                {
+                    AnimesId = table.Column<int>(type: "int", nullable: false),
+                    PalavrasChaveId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnimePalavraChave", x => new { x.AnimesId, x.PalavrasChaveId });
+                    table.ForeignKey(
+                        name: "FK_AnimePalavraChave_Animes_AnimesId",
+                        column: x => x.AnimesId,
+                        principalTable: "Animes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_AnimePalavraChave_PalavrasChave_PalavrasChaveId",
+                        column: x => x.PalavrasChaveId,
+                        principalTable: "PalavrasChave",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnimePalavraChave_PalavrasChaveId",
+                table: "AnimePalavraChave",
+                column: "PalavrasChaveId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnimePalavraChave");
+
+            migrationBuilder.DropTable(
+                name: "Animes");
+
+            migrationBuilder.DropTable(
+                name: "PalavrasChave");
+        }
+    }
+}
